d1c239e40934dcdeafc23d671efec46d4a420cb9
Richard Alexandrino de Macedo <richardalexandrino@hotmail.com>
2022-12-11 19:53:58 -0300

    Course update

--- curso_programacao_12(5)/src/application/Program01.java
+++ curso_programacao_12(5)/src/application/Program01.java
@@ -1,15 +1,11 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
 
 public class Program01 {
 
@@ -20,24 +16,31 @@ public class Program01 {
 		try {
 			conn = DB.getConnection();
 			
+			conn.setAutoCommit(false);
+			
 			st = conn.createStatement();
 			
-			int rows1 = st.executeUpdate(""
-					+ "UPDATE seller SET BaseSalary = 2090 WHERE DepartamentId = 1");
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			int rows2 = st.executeUpdate(""
-					+ "UPDATE seller SET BaseSalary = 3090 WHERE DepartamentId = 2");
+//			int x = 1;
+//			if (x<2) {
+//				throw new SQLException("Fake error");
+//			}
 			
-			int x = 1;
-			if (x<2) {
-				throw new SQLException("Fake error");
-			}
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();
 			
 			System.out.println("rows1 " + rows1);
 			System.out.println("rows2 " + rows2);
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			try {
+				conn.rollback();
+				throw new DbException("Transaction rolled back! Cause by: " + e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+			}
 		}
 		finally {
 			DB.closeStatement(st);
